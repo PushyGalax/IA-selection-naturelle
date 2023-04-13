e2214ee28d4d9b7934035a3e247a538ea815ae16
PushyGalax <galaxcraft38@gmail.com>
2023-04-13 09:28:03 +0200

    initialyse class

--- main.py
+++ main.py
@@ -1,7 +1,28 @@
 #import
 import pygame as pg
+from random import *
 
 #prg
+class victime:
+    def __init__(self) -> None:             #je pense an stat il faut taille, vitesse, pv, champ de vision
+        pass
+
+    def __str__(self) -> str:               #print des stat de l'objet
+        pass
+
+class monstre:
+    def __init__(self) -> None:             #les monstres n'évolue pas ils font 1 de dégats et ils ont vitesse const et champ de vision const
+        pass
+
+    def __str__(self) -> str:
+        pass
+
+class fruit:
+    def __init__(self) -> None:             #les fruits n'évolue pas, plus 1 pv quand victime sur fruit
+        pass
+
+    def __str__(self) -> str:
+        pass
 
 
 #main
