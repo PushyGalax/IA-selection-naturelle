c3341e83c6668d1f0bb3d7431e66d782f3be1f1b
PushyGalax <galaxcraft38@gmail.com>
2023-04-13 12:08:28 +0200

    start var global

--- main.py
+++ main.py
@@ -3,22 +3,29 @@ import pygame as pg
 from random import *
 
 #prg
-class victime:
-    def __init__(self) -> None:             #je pense an stat il faut taille, vitesse, pv, champ de vision
-        pass
+class IA:
+    def __init__(self, vitesse, taille, champvision, pv) -> None:             #je pense an stat il faut taille, vitesse, pv, champ de vision
+        self.vitesse = vitesse
+        self.taille = taille
+        self.champvision = champvision
+        self.pv = pv
 
     def __str__(self) -> str:               #print des stat de l'objet
+        return f"{self.vitesse} {self.taille} {self.champvision}"
+    
+    def move(self):
         pass
 
 class monstre:
     def __init__(self) -> None:             #les monstres n'évolue pas ils font 1 de dégats et ils ont vitesse const et champ de vision const
-        pass
+        self.VITESSE = 20                   #val à changer
+        self.CHAMPVISION = 35
 
     def __str__(self) -> str:
         pass
 
 class fruit:
-    def __init__(self) -> None:             #les fruits n'évolue pas, plus 1 pv quand victime sur fruit
+    def __init__(self) -> None:             #les fruits n'évolue pas, plus 1 pv quand IA sur fruit
         pass
 
     def __str__(self) -> str:
