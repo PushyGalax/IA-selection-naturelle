581b25f980e627671b823e29efefacd1b22a8e6c
InspireFREZZY <val.spg38@gmail.com>
2023-04-22 09:57:10 +0200

    reupdate stats

--- main.py
+++ main.py
@@ -44,14 +44,21 @@ class IA(pg.sprite.Sprite):
         self.pv-=1
         self.pv_text = police.render(str(self.pv)+" PV", 1, (255,)*3)
         self.imortality_frames = default_timer()
-    
+
+    def bonus(self):
+        self.pv+=1
+        if self.pv < self.pvmax:
+            self.pvmax = self.pv
+        self.pv_text = police.render(str(self.pv)+" PV", 1, (255,)*3)
+
     def fin(self):
         time = default_timer()
         self.timer = time - self.timer
         return [self.vitessenum, self.TAILLE, self.champvision, self.pvmax, self.timer]
 
-    def move(self, monstres):
-        self.recherche_plus_proches(monstres)
+    def move(self, monstres, fruits):
+        self.recherche_plus_proche_monstre(monstres)
+        self.recherche_plus_proche_fruit(fruits)
         if default_timer() - self.change_direction_timer > 1:
             self.vector = pg.Vector2(choice((-random(), random())), choice((-random(), random())))
             self.vector = self.vector.normalize()
@@ -72,14 +79,19 @@ class IA(pg.sprite.Sprite):
             self.rect.centery = self.TAILLE//2
             self.vector.y *= -1
 
-    def collisionmonstre(self, monstre):
-        if self.rect.colliderect(monstre.rect):
+    def collision_monstre(self, monstre):
+        if default_timer()-self.imortality_frames>=self.hit_cooldown and self.rect.colliderect(monstre.rect):
             self.degat()
+
+    def collision_fruit(self, fruit):
+        if self.rect.colliderect(fruit.rect):
+            group_fruits.remove(fruit)
+            self.bonus()
 
     def distance(self, point): # point = classe avec un rect
         return sqrt((self.rect.centerx-point.rect.x)**2 + (self.rect.centery-point.rect.y)**2)
 
-    def recherche_plus_proches(self, monstres):
+    def recherche_plus_proche_monstre(self, monstres):
         min_dist = float("+inf")
         monstre_proche = None
         for elt in monstres.sprites():
@@ -88,9 +100,20 @@ class IA(pg.sprite.Sprite):
                 min_dist = dist_monstre
                 monstre_proche = elt
         if monstre_proche is not None:
-            self.colisonmonstre(monstre_proche)
+            self.collision_monstre(monstre_proche)
             self.vector = pg.Vector2(self.rect.centerx-monstre_proche.rect.centerx, self.rect.centery-monstre_proche.rect.centery)
             self.vector = self.vector.normalize()
+
+    def recherche_plus_proche_fruit(self, fruits):
+        min_dist = float("+inf")
+        fruit_proche = None
+        for elt in fruits.sprites():
+            dist_fruit = self.distance(elt)
+            if dist_fruit<=self.champvision and dist_fruit<min_dist:
+                min_dist = dist_fruit
+                fruit_proche = elt
+        if fruit_proche is not None:
+            self.collision_fruit(fruit_proche)
 
 
 class Monstre(pg.sprite.Sprite):
@@ -127,9 +150,11 @@ class Monstre(pg.sprite.Sprite):
 class fruit(pg.sprite.Sprite):
     def __init__(self) -> None:             #les fruits n'évolue pas, plus 1 pv quand IA sur fruit
         super().__init__()
-
-    def __str__(self) -> str:
-        pass
+        self.image = pg.image.load("fruit.png")
+        self.image.set_colorkey((245,)*3)
+        self.image = pg.transform.scale(self.image, (50,50))
+        self.rect = self.image.get_rect()
+        self.rect.center = [randint(0,1280), randint(0,720)]
 
 
 #main
@@ -144,21 +169,31 @@ running = True
 police = pg.font.SysFont("monospace" ,15)
 
 # fruit
-group_fruit = pg.sprite.Group()
-group_fruit.add(fruit())
+group_fruits = pg.sprite.Group()
+for i in range(5):
+    group_fruits.add(fruit())
 
 # monstre
 group_monstre = pg.sprite.Group()
-for i in range(10):
+for i in range(20):
     new_monstre = Monstre()
     group_monstre.add(new_monstre)
 
 ia_group = pg.sprite.Group()
 
 # ia
-for joueur in range(5):
+for joueur in range(12):
     new_player = IA(2, 30, 200, 3)
     ia_group.add(new_player)
+
+# les stats
+screen.fill("#A0A0A0", (1280,0,1780,720))
+bordure = pg.image.load("bordure.png")
+bordure = pg.transform.scale(bordure, (16,720))
+texte_stats = pg.image.load("texte_stats.png")
+texte_stats = pg.transform.scale(texte_stats, (256,64))
+screen.blit(bordure, (1280,0))
+screen.blit(texte_stats, (1410,50))
 
 statia=[]
 
@@ -168,14 +203,13 @@ while running:
             running = False
     
     screen.fill("black", (0,0,1280,720))
-    screen.fill("white", (1280,0,1780,720))
 
     for elt in group_monstre.sprites():
         elt.move()
 
     for elt in ia_group.sprites():
-        elt.move(group_monstre)
-        screen.blit(elt.pv_text, (elt.rect.x, elt.rect.centery-elt.TAILLE))
+        elt.move(group_monstre, group_fruits)
+        screen.blit(elt.pv_text, (elt.rect.x-8, elt.rect.centery-elt.TAILLE))
 
     ia_list = ia_group.sprites()
     if len(ia_list) != 0:
@@ -186,11 +220,6 @@ while running:
                 mort=elem.fin()
                 statia.append(mort)
                 ia_group.remove(elem)
-
-        for elem in ia_list:
-            alea = randint(1,5)
-            if alea == 1:
-                elem.degat()
     else:
         statia.sort(key=lambda M : M[4], reverse=True)
         best=statia[0]
@@ -204,9 +233,10 @@ while running:
         ia_group.add(IA(best[0],best[1],best[2],best[3]))
         statia = []
 
+    group_fruits.draw(screen)
     ia_group.draw(screen)
     group_monstre.draw(screen)
-
+    
     pg.display.flip()
 
     clock.tick(60)
